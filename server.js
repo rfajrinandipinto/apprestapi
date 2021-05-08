433d0b75b5417b51421b78577f44be8577e9b7ae
R Fajri Nandipinto <nandipintorahmatfajri@gmail.com>
2021-05-08 13:55:44 +0700

    setting routes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
