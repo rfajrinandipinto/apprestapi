bfcaa79c2469e762ec7da3d1cca75c60aea377ab
R Fajri Nandipinto <nandipintorahmatfajri@gmail.com>
2021-05-08 16:40:16 +0700

    berhasil menghapus data

--- controller.js
+++ controller.js
@@ -68,3 +68,19 @@ exports.ubahmhs = function(req,res){
             }
         });
 };
+
+
+//Menghapus data berdasarkan id
+exports.hapusmhs = function(req,res){
+    var id = req.body.id_mahasiswa;
+
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+    function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }
+        else{
+            response.ok("Berhasil Menghapus Data",res)
+        }
+    });
+};
